5b06aea80ef3fe9c76c3e53c1c2a4cb562b51c9d
anaschran-del <ana.schran@escola.pr.gov.br>
2026-06-08 13:54:42 +0000

    salvar

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def inicio():
+    nome = "Turma de Programação"
+    curso = "Phyton com HTML"
+
+    return render_template(
+        'index.html'
+        nome = nome,
+        curso =
+    )
